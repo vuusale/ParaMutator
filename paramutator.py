ff99b6602c52fa780f0a581210779bd1410e984b
Your Name <you@example.com>
2024-09-02 09:13:48 -0400

    Support for XML and nested JSON body added

--- paramutator.py
+++ paramutator.py
@@ -1,5 +1,7 @@
+import xml
 import sys
 import json
+import copy
 import argparse
 import requests
 from urllib3.exceptions import InsecureRequestWarning
@@ -9,14 +11,14 @@ requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
 modifiers = [
     # parameter name modifiers
-    {"param": "{}[]", "value": "{}"}, # array injection
-    {"param": "{}'", "value": "{}"}, # special char in param name
+    # {"param": "{}[]", "value": "{}"}, # array injection
+    # {"param": "{}'", "value": "{}"}, # special char in param name
 
     # parameter value modifiers
     {"value": [1,2,3], "param": "{}", "types": ["str", "int"]}, # array in field with other data type
     {"value": {"a": 1}, "param": "{}", "types": ["str", "int"]}, # object in field with other data type
-    {"value": str(10**4000), "param": "{}"}, # long string
-    {"value": 2**7000, "param": "{}"}, # large integer
+    # {"value": str(10**4000), "param": "{}"}, # long string
+    # {"value": 2**7000, "param": "{}"}, # large integer
     {"value": "😁😛😋🤣", "param": "{}"}, # emojis
     {"value": "\\u0000\\u0007\\u0008\\u0009\\u000a\\u000b\\u000c\\u000d\\u001b\\u005c", "param": "{}"}, # null byte and escape sequences in unicode
     {"value": "\\x00\\x07\\x08\\x09\\x0a\\x0b\\x0c\\x0d\\x1b\\x5c", "param": "{}"}, # null byte and escape sequences in hex
@@ -40,28 +42,77 @@ class RequestHandler:
     def handle(self, part_to_modify):
         """Modifies request parts and sends requests"""
 
-        backup_part = getattr(self, part_to_modify, None)
-        if not isinstance(backup_part, dict):
+        original_part = getattr(self, part_to_modify, None)
+        if not isinstance(original_part, dict):
             return
-            
-        for param, value in backup_part.items():
-            value_datatype = identify_datatype(value)
+
+        if isinstance(original_part, xml.etree.ElementTree.Element):
+            self.xml_handler(original_part)
+
+        else:
+            self.json_handler(original_part, part_to_modify)
+
+    def xml_handler(self, original_part):
+        for child in original_part.iter():
             for modifier in modifiers:
-                if value_datatype not in modifier.get("types", [value_datatype]):
-                    continue
+                modified_part = copy.deepcopy(original_part)
+                inner_text = child.text
+                attributes = child.attrib
+                tag = child.tag
+                print(f"Before:\t{tag} = {inner_text}")
+                child.text = modifier["value"].format(inner_text) if isinstance(modifier["value"], str) else str(modifier["value"])
+                print(f"Now:\t{tag} = {child.text}")
+                self.body = xml.etree.ElementTree.tostring(original_part)
+                self.send_request()
+                child.text = inner_text
 
-                modified_part = backup_part.copy()
-                modified_param = modifier["param"].format(param) if isinstance(modifier["param"], str) else modifier["param"]
-                modified_value = modifier["value"].format(value) if isinstance(modifier["value"], str) else modifier["param"]
+                if attributes:
+                    for attribute, value in attributes.items():
+                        print(f"Before:\t{tag} {attribute} = {value}")
+                        child.attrib[attribute] = modifier["value"].format(value) if isinstance(modifier["value"], str) else str(modifier["value"])
+                        print(f"Now:\t{tag} {attribute} = {child.attrib[attribute]}")
+                        self.body = xml.etree.ElementTree.tostring(original_part)
+                        self.send_request()
+                        child.attrib[attribute] = value
 
-                del modified_part[param]
-                modified_part[modified_param] = modified_value
-                setattr(self, part_to_modify, modified_part)
-                print(f"Before: {backup_part}. Now: {getattr(self, part_to_modify)}")
-                self.send_request(modifier, part_to_modify)
+    def apply_modifier(self, entire_parameter, param, value):   
+        value_datatype = identify_datatype(value)
+        entire_parameter_datatype = identify_datatype(entire_parameter)
+        for modifier in modifiers:
+            modified_param = param if entire_parameter_datatype == "list" else modifier["param"].format(param) if isinstance(modifier["param"], str) else modifier["param"]
+            modified_value = modifier["value"].format(value) if isinstance(modifier["value"], str) else modifier["value"]
+            entire_parameter[modified_param] = modified_value
+            yield entire_parameter
+            entire_parameter[modified_param] = value
+        
+        match value_datatype:
+            case "dict":
+                for inner_param, inner_value in value.items():
+                    modified_parameters = self.apply_modifier(value, inner_param, inner_value)
+                    for modified_parameter in modified_parameters:
+                        entire_parameter[param] = modified_parameter
+                        yield entire_parameter
+                        entire_parameter[modified_param] = value
+            case "list":
+                for index in range(len(value)):
+                    item = value[index]
+                    modified_parameters = self.apply_modifier(value, index, item)
+                    for modified_parameter in modified_parameters:
+                        entire_parameter[param] = modified_parameter
+                        yield entire_parameter
+                        entire_parameter[param] = item
+
+    def json_handler(self, original_part, part_to_modify):
+        for param, value in original_part.items():
+            backup_part = original_part.copy()
+            modified_parameters = self.apply_modifier(backup_part, param, value)
+            for modified_parameter in modified_parameters:
+                setattr(self, part_to_modify, modified_parameter)
+                print(f"[+] Before: {original_part}.\n[+] Now: {getattr(self, part_to_modify)}")
+                self.send_request()
                 setattr(self, part_to_modify, backup_part)
 
-    def send_request(self, modifier, part_to_modify):
+    def send_request(self):
         """Sends the HTTP request with the modified parameters"""
 
         request_args = {
@@ -78,7 +129,7 @@ class RequestHandler:
             request_args["json"] = self.body
         else:
             request_args["data"] = self.body
-        
+
         try:
             response = requests.request(**request_args)
             print("Response Code:", response.status_code)
